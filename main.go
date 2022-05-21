c905aa5c5bcf638c44c3df463ad6c5301288dd36
Artur Muradov <muradov86@gmail.com>
2022-05-21 10:42:48 +0300

    data types printing was added

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
+	fmt.Printf("\nconferenceName is %T, conferenceTickets is %T, remainingTickets is %T\n\n", conferenceName, conferenceTickets, remainingTickets)
+
 	fmt.Printf("Welcome to %s booking application\n", conferenceName)
 	fmt.Printf("We have total of %d tickets and %d are still available\n", conferenceTickets, remainingTickets)
-	fmt.Printf("Get your tickets here to attend")
+	fmt.Printf("Get your tickets here to attend\n")
 
 }
